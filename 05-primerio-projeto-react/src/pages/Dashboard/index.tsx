3af4c557b9932be3efba210783d152019f95d5fc
Mikael Josias <mikaeljosias2002@gmail.com>
2021-01-18 12:37:17 -0300

    feat(repository): navigation by routes

--- 05-primerio-projeto-react/src/pages/Dashboard/index.tsx
+++ 05-primerio-projeto-react/src/pages/Dashboard/index.tsx
@@ -3,6 +3,7 @@ import {Title, Form, Repositories, Error} from './style';
 import logoImg from '../../assets/logo.svg';
 import {FiChevronRight} from 'react-icons/fi';
 import api from '../../services/api';
+import {Link} from 'react-router-dom';
 
 
 interface Repository{
@@ -72,7 +73,7 @@ const Dashboard: React.FC = () => {
 
             <Repositories>
                 {repositories.map(repository => (
-                    <a key={repository.full_name} href="#teste">
+                    <Link key={repository.full_name} to={`/repositories/${repository.full_name}`}>
                         <img 
                             src={repository.owner.avatar_url}
                             alt={repository.owner.login}
@@ -83,7 +84,7 @@ const Dashboard: React.FC = () => {
                         </div>
 
                         <FiChevronRight size={20}/>
-                    </a>
+                    </Link>
                 ))}
             </Repositories>
         </>
